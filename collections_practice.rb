1fa7a8d73a8d7133ad07b75e23d87989185fd940
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-26 00:37:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.each {|word| return false if word[0] != "r"}
+  true
+end
+
+def contain_a(arr)
+  a_arr = []
+  arr.each {|word| a_arr << word if word.include? "a"}
+  a_arr
+end
+
+def first_wa(arr)
+  arr.each do |word|
+    if word[0] == "w"
+      return word if word[1] == "a"
+    end
+  end
+end
+
+def remove_non_strings(arr)
+end
+
+def count_elements(hash)
+end
+
+def merge_data(hash1, hash2)
+end
+
+def find_cool(hash)
+end
+
+def organize_schools(hash)
+end
